23832fb43d4bc8621b78f6ed85e7aac6fe0c5cc7
kimnguyen4497 <kimnguyen4497@gmail.com>
2021-10-14 16:26:12 -0700

    calculator

---
+++ calculator.py
@@ -0,0 +1,29 @@
+
+def calculator(number1, number2, operator):
+
+    if operator == '*':
+        return number1 * number2
+    elif operator == '+':
+        return number1 + number2
+    elif operator == '**':
+        return number1 ** number2
+    elif operator == '/':
+        if number2 == 0:
+            return 'False'
+        return number1 / number2
+    elif operator == '//':
+        if number2 == 0:
+            return 'False'
+        return number1 // number2
+    elif operator == '-':
+        return number1 - number2
+
+    
+    
+def parse_input():
+    user_input = input ("Enter equation: ")
+    user_input = user_input.strip().split(" ")
+
+    number1 = float(user_input[0])
+    number2 = float(user_input[2])
+    operator = user_input[1]
